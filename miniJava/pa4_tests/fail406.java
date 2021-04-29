32552608e1a5f75e79c5e8942f1649344ff21468
CharlieZhang1999 <qiuyang@live.unc.edu>
2021-04-29 05:39:29 -0400

    Start point for PA5

--- miniJava/pa4_tests/fail406.java
+++
@@ -1,7 +0,0 @@
-/*** sourcefile lacks a suitable "main" method
- * COMP 520
- * Identification
- */
-class fail25 { 	
-	public void main(String [] args) {}
-}
--- miniJava/pa4_tests/fail407.java
+++
@@ -1,9 +0,0 @@
-/*** sourcefile lacks a suitable "main" method
- * COMP 520
- * Type checking
- */
-class fail27 { 	
-    public static int main(String [] args) { 
-        return 3;
-    }
-}
